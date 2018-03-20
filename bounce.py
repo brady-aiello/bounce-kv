d9811bfc85306346fe1a3507272cacf38b81d6f1
brady-aiello <bbaiello@calpoly.edu>
2018-03-19 19:18:27 -0700

    Got bounce working 2.
    Signed-off-by: brady-aiello <bbaiello@calpoly.edu>

--- bounce.py
+++ bounce.py
@@ -2,13 +2,13 @@ from Tkinter import *
 import random
 import time
 
-# tk = Tk()
-# tk.title("BOUNCE,THE BEST GAME EVER!")
-# tk.resizable(0, 0)
-# tk.wm_attributes("-topmost", 1)
-# canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
-# canvas.pack()
-# tk.update()
+tk = Tk()
+tk.title("BOUNCE,THE BEST GAME EVER!")
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
 
 
 class Ball:
@@ -18,16 +18,16 @@ class Ball:
         #self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
-        # self.canvas_width = self.canvas.winfo_width()
-        # starts = [-3, -2, -1, 1, 2, 3]
-        # random.shuffle(starts)
-        # self.x = starts[0]
-        # self.y = -3
-        # self.canvas_height = self.canvas.winfo_height()
-        # self.x = 0
-        # self.y = -1
-        # self.canvas_width = self.canvas.winfo_width()
-        # self.hit_bottom = False
+        self.canvas_width = self.canvas.winfo_width()
+        starts = [-3, -2, -1, 1, 2, 3]
+        random.shuffle(starts)
+        self.x = starts[0]
+        self.y = -3
+        self.canvas_height = self.canvas.winfo_height()
+        self.x = 0
+        self.y = -1
+        self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
         
     def draw(self):
         self.canvas
