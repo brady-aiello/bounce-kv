5ba60a2567fc29abf9d313085d8517297bf74494
brady-aiello <bbaiello@calpoly.edu>
2018-03-19 18:56:02 -0700

    Got ball bouncing.
    Signed-off-by: brady-aiello <bbaiello@calpoly.edu>

--- bounce.py
+++ bounce.py
@@ -2,37 +2,41 @@ from Tkinter import *
 import random
 import time
 
-tk = Tk()
-tk.title("BOUNCE,THE BEST GAME EVER!")
-tk.resizable(0, 0)
-tk.wm_attributes("-topmost", 1)
-canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
-canvas.pack()
-tk.update()
+# tk = Tk()
+# tk.title("BOUNCE,THE BEST GAME EVER!")
+# tk.resizable(0, 0)
+# tk.wm_attributes("-topmost", 1)
+# canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+# canvas.pack()
+# tk.update()
+
 
 class Ball:
-   
+
     def __init__(self, canvas, color):
         self.canvas = canvas
         #self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
-        self.canvas_width = self.canvas.winfo_width()
-        starts = [-3, -2, -1, 1, 2, 3]
-        random.shuffle(starts)
-        self.x = starts[0]
-        self.y = -3
-        self.canvas_height = self.canvas.winfo_height()
-        self.canvas_width = self.canvas.winfo_width()
-        self.hit_bottom = False
+        # self.canvas_width = self.canvas.winfo_width()
+        # starts = [-3, -2, -1, 1, 2, 3]
+        # random.shuffle(starts)
+        # self.x = starts[0]
+        # self.y = -3
+        # self.canvas_height = self.canvas.winfo_height()
+        # self.x = 0
+        # self.y = -1
+        # self.canvas_width = self.canvas.winfo_width()
+        # self.hit_bottom = False
         
     def draw(self):
-        self.canvas.move(self.id, 0, -1)
+        self.canvas
+
+
+        self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
         if pos[1] <= 0:
             self.y = 3
-       # if self.hit_paddle(pos) == True:
-            #self.y = -3
         if pos[3] >= self.canvas_height:
             self.y = -3
         if pos[0] <= 0:
