ddbb73adb1b7217ba78b488655689f2f8bb35e10
brady-aiello <bradizl@gmail.com>
2018-03-23 19:20:46 -0700

    got the ball bouncing in different directions and added some paddle code.
    Signed-off-by: brady-aiello <bradizl@gmail.com>

--- bounce.py
+++ bounce.py
@@ -1,38 +1,23 @@
 from Tkinter import *
 import random
 import time
-
-tk = Tk()
-tk.title("BOUNCE,THE BEST GAME EVER!")
-tk.resizable(0, 0)
-tk.wm_attributes("-topmost", 1)
-canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
-canvas.pack()
-tk.update()
-
-
 class Ball:
 
     def __init__(self, canvas, color):
         self.canvas = canvas
         #self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
-        self.canvas.move(self.id, 245, 100)
-        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.move(self.id, 200, 300)
         starts = [-3, -2, -1, 1, 2, 3]
         random.shuffle(starts)
         self.x = starts[0]
         self.y = -3
         self.canvas_height = self.canvas.winfo_height()
-        self.x = 0
-        self.y = -1
         self.canvas_width = self.canvas.winfo_width()
         self.hit_bottom = False
         
     def draw(self):
         self.canvas
-
-
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
         if pos[1] <= 0:
@@ -43,11 +28,39 @@ class Ball:
             self.x = 3
         if pos[2] >= self.canvas_width:
             self.x = -3
+tk = Tk()
+tk.title("BOUNCE,THE BEST GAME EVER!")
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
+
+
+
+
+class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+
+
+    def draw(self):
+        def turn_left(self, evt):
+            self.x = -2
+
+        def turn_right(self, evt):
+            self.x = 2
 
 ball = Ball(canvas, "red")
+paddle = Paddle(canvas, 'blue')
 
 while 1:
     ball.draw()
+    paddle.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
